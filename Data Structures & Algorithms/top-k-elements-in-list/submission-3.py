57148480fba0a18f83ea485b149d082801b4647a
noahpi965 <noahpi965@gmail.com>
2026-09-15 17:04:23 -0400

    Update: top-k-elements-in-list - submission-3

--- Data Structures & Algorithms/top-k-elements-in-list/submission-3.py
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.py
@@ -1,16 +1,16 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {} #第一次map,   k=数值,v=频次
-        freq = [ [] for i in range(len(nums)+1)] # 创建一个二维数据
-
-        for n in nums:
-            count[n] = 1 + count.get(n, 0)
-        for n, c in count.items():# 用第一次map组装二维数组
-            freq[c].append(n)  # 这时候 数组下角标是频次, val是有频次的数
+        count = {} # first val-freq map
+        freq = [ [] for i in range(len(nums) + 1) ] # set buckets all freqs 
+        
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        for num, cunt in count.items():
+            freq[cunt].append(num)
 
         res = []
-        for i in range(len(freq)-1,0,-1): # freq从后往头数
-            for n in freq[i]:
-                res.append(n)# 把高频词数装进res里去
-                if len(res) == k: # 装到要求的数量k了
+        for i in range(len(freq) -1, 0, -1): # from the end of freq to start
+            for num in freq[i]: # every index arr of freq,foreach its int
+                res.append(num) # res insert most freq int
+                if len(res) == k: # res enough
                     return res
